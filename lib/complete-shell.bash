de594aa76ece8e02fcd535204ca1e46c615f9e1f
Ingy döt Net <ingy@ingy.net>
2015-10-14 23:11:12 +0000

    Refactoring.

--- lib/complete-shell.bash
+++ lib/complete-shell.bash
@@ -1,18 +1,5 @@
 #!bash
 
-complete-shell:activate-all() {
-  local all=(
-    $(git config -f $COMPLETE_SHELL_ROOT/config --get-regexp complete-shell)
-  )
-  set -- "${all[@]}"
-  while [[ $# -gt 0 ]]; do
-    local cmd_name="${1#complete-shell.}" bash_file="$2"
-    shift; shift
-    source $bash_file || continue
-    complete -F complete-shell:add-completion "$cmd_name"
-  done
-}
-
 complete-shell:add-completion() {
   local cmd="$1" word="$2" prev="$3"
   set -- $("complete-shell::$cmd")
