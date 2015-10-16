7cb1aaedf50e171940577e92c5140d3a534512e3
Ingy döt Net <ingy@ingy.net>
2015-10-16 14:15:01 -0700

    Get doc up to current state.

--- lib/complete-shell-activate.bash
+++ lib/complete-shell-activate.bash
@@ -1,33 +1,45 @@
 #! bash
 
+# This compdef command loads the version specific runtime for the other
+# commands:
 .complete-shell() {
   local version="${1#v}"
   [[ $version =~ [0-9]+\.[0-9]+\.[0-9]+ ]] || {
-    echo "'$complete_file' has '.complete-shell' directive with invalid version"
+    cat <<...
+Bad header directive in '$complete_file'.
+The '.complete-shell' has a invalid version '$version'.
+...
     return 1
   }
   source "$COMPLETE_SHELL_ROOT/lib/complete-shell-$version.bash"
 }
 
+# Activate a compdef file:
 complete-shell-activate() {
-  PATH= source "$1"
-  [[ $(type -t .DESTROY) != function ]] || .DESTROY
+  local rc=0
+  PATH= source "$1" || rc=$?
+  if [[ $(type -t .DESTROY) == function ]]; then
+    .DESTROY
+  fi
+  return $rc
 }
 
+# Activate all the compdef files in the user's config file:
 complete-shell-activate-all() {
-  IFS=' '$'\t'$'\n'
+  IFS=' '$'\t'$'\n'   # Reset IFS just in case
   local all=(
     $(git config -f $COMPLETE_SHELL_ROOT/.config --get-regexp complete-shell)
   )
   set -- "${all[@]}"
   while [[ $# -gt 0 ]]; do
-    local cmd_name="${1#complete-shell.}" complete_file="$2"
-    shift; shift
+    shift
+    local complete_file="$1"
+    shift
     complete-shell-activate "$complete_file"
   done
 }
 
-[[ $0 == $BASH_SOURCE ]] || {
+[[ $0 == $BASH_SOURCE && $# -gt 0 ]] || {
   command="$1"; shift
   "$command" "$@"
 }
@@ -36,3 +48,5 @@ unset -f \
   .complete-shell \
   complete-shell-activate \
   complete-shell-activate-all
+
+# vim: set ft=sh lisp:
---
+++ test/source.t
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+# Use test::more
+
+# source all bash files
